0203a8ff90863963b6ed5d93f318825408dd50ff
DmytroPetrenko <44116687+DmytroPetrenko@users.noreply.github.com>
2021-06-14 17:51:11 +0300

    delete socket form img

--- components/MyImage.vue
+++ components/MyImage.vue
@@ -1,33 +1,10 @@
 <template>
   <v-avatar class="ma-0" :size="size" tile>
-    <img :id="user.id" src="" />
+    <img :src="`http://localhost:3000/${user.imgSrc}`" />
   </v-avatar>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      imgChunks: []
-    };
-  },
-  computed: {
-    img: function() {
-      return this.$el.querySelector(`#${this.user.id}`);
-    }
-  },
-  props: ["size", "user"],
-  sockets: {
-    sendChunk(chunk) {
-      //const img = this.$el.querySelector(`#${this.user.id}`);
-      this.imgChunks.push(chunk);
-      this.img.setAttribute(
-        "src",
-        "data:image/png;base64," + window.btoa(this.imgChunks)
-      );
-    }
-  },
-  mounted() {
-    this.$socket.emit("getUserImg", this.user.imgSrc);
-  }
+  props: ["size", "user"]
 };
 </script>
